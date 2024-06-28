242291b4b1f9c02c129fad2128131971edd9d571
tuxbox <nathan.flow@gmail.com>
2024-06-27 21:41:15 -0500

    adding a run from web script

---
+++ setup.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Sets everything up from the web
+
+
+#get redis server
+#setup redis server
+
+#Setup steam?
+#Setup Arma 3 dedicated server
+#setup Arma 3 desktop version
+
+#get Epoch server from web
+#get Arma 3 workshop items, ie epoch client
+
