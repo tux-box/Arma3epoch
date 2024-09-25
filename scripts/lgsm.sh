80e642da74c41d82dd7d9c8de8baa45f08f60ddd
tux-box <nathan.flow@gmail.com>
2024-09-24 20:57:32 -0500

    Update lgsm.sh

--- scripts/lgsm.sh
+++ scripts/lgsm.sh
@@ -3,45 +3,46 @@
 
 ${FORCE_INSTALL_DIR}=/data/serverfiles
 
+
 APP_UPDATE="107410"
 WORKSHOP_DOWNLOAD_ITEM="421839251"
 MOD_NAME="@epoch"
-steamcmd +force_install_dir ${FORCE_INSTALL_DIR} +login ${USERNAME} ${PASSWRD} +app_update ${APP_UPDATE} +workshop_download_item 107410 421839251 validate +quit
+steamcmd +force_install_dir ${FORCE_INSTALL_DIR} +login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} +app_update ${APP_UPDATE} +workshop_download_item 107410 421839251 validate +quit
 echo ---Creating Symbolic link for ${MOD_NAME}----
 ln -s  /serverdata/serverfiles/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
 
 DESKTOP_ARMA_ID="107410"
 MOD_ID="583496184"
 MOD_NAME="@CUP Terrains - Core"
-${STEAMCMD_DIR}/steamcmd +login ${USERNAME} ${PASSWRD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
+${STEAMCMD_DIR}/steamcmd +login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
 echo ---Creating Symbolic link for ${MOD_NAME}----
 ln -s  /serverdata/serverfiles/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
 
 DESKTOP_ARMA_ID="107410"
 MOD_ID="583544987"
 MOD_NAME="@CUP Terrains - Maps"
-${STEAMCMD_DIR}/steamcmd +login ${USERNAME} ${PASSWRD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
+${STEAMCMD_DIR}/steamcmd +login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
 echo ---Creating Symbolic link for ${MOD_NAME}----
 ln -s  /serverdata/serverfiles/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
 
 DESKTOP_ARMA_ID="107410"
 MOD_ID="541888371"
 MOD_NAME="@CUP Vehicles"
-${STEAMCMD_DIR}/steamcmd +login ${USERNAME} ${PASSWRD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
+${STEAMCMD_DIR}/steamcmd +login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
 echo ---Creating Symbolic link for ${MOD_NAME}----
 ln -s  /serverdata/serverfiles/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
 
 DESKTOP_ARMA_ID="107410"
 MOD_ID="450814997"
 MOD_NAME="@CBA_A3"
-${STEAMCMD_DIR}/steamcmd +login ${USERNAME} ${PASSWRD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
+${STEAMCMD_DIR}/steamcmd +login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
 echo ---Creating Symbolic link for ${MOD_NAME}----
 ln -s  /serverdata/serverfiles/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
 
 DESKTOP_ARMA_ID="107410"
 MOD_ID="497660133"
 MOD_NAME="@CUP Weapons"
-${STEAMCMD_DIR}/steamcmd +login ${USERNAME} ${PASSWRD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
+${STEAMCMD_DIR}/steamcmd +login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} +app_update ${DESKTOP_ARMA_ID} +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} +quit
 echo ---Creating Symbolic link for ${MOD_NAME}----
 ln -s  /serverdata/serverfiles/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
 
@@ -49,7 +50,7 @@ DESKTOP_ARMA_ID="107410"
 MOD_ID="497661914"
 MOD_NAME="@CUP Units"
 ${STEAMCMD_DIR}/steamcmd \
-+login ${USERNAME} ${PASSWRD} \
++login ${STEAM_USER_NAME} ${STEAM_USER_PASSWORD} \
 +app_update ${DESKTOP_ARMA_ID} \
 +workshop_download_item ${DESKTOP_ARMA_ID} ${MOD_ID} validate \
 +quit
