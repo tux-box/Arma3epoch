0a3ef02f6ad088bf7cdf7eef55fb024105a901b6
tux-box <nathan.flow@gmail.com>
2025-01-08 15:20:47 -0600

    updated to match new file paths

--- scripts/getEpochServer.sh
+++ scripts/getEpochServer.sh
@@ -2,6 +2,6 @@
 #downloads the epoch server files
 git clone https://github.com/EpochModTeam/Epoch.git $STEAMCACHE_DIR/epoch_serv_src
 #make the files all lower case for uniformity.
-/bin/bash $HOME/scripts/makeLowerCase.sh $STEAMCACHE_DIR/epoch_serv_src
-cp -r $STEAMCACHE_DIR/server_install_pack/* ${FORCE_INSTALL_DIR}
+/bin/bash $HOME/source/scripts/makeLowerCase.sh $STEAMCACHE_DIR/epoch_serv_src
+cp -r $STEAMCACHE_DIR/epoch_serv_src/server_install_pack/* ${FORCE_INSTALL_DIR}
 #rm -r -f /tmp/epoch_serv_src
