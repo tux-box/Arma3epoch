3e6604ef8a5c516f96aa648971aed02aa79cee1b
tux-box <nathan.flow@gmail.com>
2025-01-08 14:59:04 -0600

    adding quotes around modname to handle spaces I hope

--- scripts/steam/installModsByCSV.sh
+++ scripts/steam/installModsByCSV.sh
@@ -15,6 +15,6 @@ while IFS=';' read -r MOD_ID MOD_NAME; do
  +workshop_download_item ${WORKSHOP_ID} ${MOD_ID} validate \
  +quit
  echo ---Creating Symbolic link for $MOD_NAME
- ln -s  ${STEAM_CACHE}/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/${MOD_NAME}
+ ln -s  ${STEAM_CACHE}/steamapps/workshop/${MOD_ID} ${FORCE_INSTALL_DIR}/"${MOD_NAME}"
  printf "\n"
 done < $HOME/source/scripts/steam/modlist.csv
