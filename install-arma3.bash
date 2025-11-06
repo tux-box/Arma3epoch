edc687bc9c52d62385d3590861e66e065783ae61
tux-box <nathan.flow@gmail.com>
2025-11-05 19:58:35 -0600

    corrected error in expression

--- install-arma3.bash
+++ install-arma3.bash
@@ -5,4 +5,4 @@ set -e  # Exit immediately if a command fails
 source "$(dirname "$0")/config.env"
 source "$(dirname "$0")/secrets.env"
 
-steamcmd +force_install_dir "$INSTALL_DIR" +login "$STEAMid" "$STEAMpass=" +app_update 107410 +quit
+steamcmd +force_install_dir "$INSTALL_DIR" +login "$STEAMid" "$STEAMpass" +app_update 107410 +quit
