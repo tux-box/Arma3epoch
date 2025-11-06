526f3040a808a9ee9106e14912b52d6bf4673024
tux-box <nathan.flow@gmail.com>
2025-11-05 19:00:33 -0600

    added steamcmd

--- rawContainerSetup.bash
+++ rawContainerSetup.bash
@@ -3,6 +3,11 @@
 #   source <(curl -s https://raw.githubusercontent.com/tux-box/Arma3epoch/refs/heads/main/rawContainerSetup.bash)
 
 apt-get update && apt-get dist-upgrade -y
-apt-get install -y nano openssh-server
+apt-get install -y nano openssh-server software-properties-common
+dpkg --add-architecture i386
+add-apt-repository multiverse
+apt-get update
+apt-get install steamcmd
+
 ssh-keygen -q -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
 curl https://github.com/tux-box.keys >> ~/.ssh/authorized_keys
