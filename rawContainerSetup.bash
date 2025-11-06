3214e1126a1137d825929161c8795817a84e2ebd
tux-box <nathan.flow@gmail.com>
2025-11-05 19:01:50 -0600

    added git, moved some things around.

--- rawContainerSetup.bash
+++ rawContainerSetup.bash
@@ -2,12 +2,10 @@
 #First make sure curl is installed then run this in terminal like so:
 #   source <(curl -s https://raw.githubusercontent.com/tux-box/Arma3epoch/refs/heads/main/rawContainerSetup.bash)
 
-apt-get update && apt-get dist-upgrade -y
-apt-get install -y nano openssh-server software-properties-common
 dpkg --add-architecture i386
 add-apt-repository multiverse
-apt-get update
-apt-get install steamcmd
+apt-get update && apt-get dist-upgrade -y
+apt-get install -y nano openssh-server software-properties-common git steamcmd
 
 ssh-keygen -q -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
 curl https://github.com/tux-box.keys >> ~/.ssh/authorized_keys
