240f8bfa1dde408462fa6097d987a3b05b5ad2cc
tux-box <nathan.flow@gmail.com>
2025-11-05 18:54:02 -0600

    added ssh keys

--- rawContainerSetup.bash
+++ rawContainerSetup.bash
@@ -1,2 +1,8 @@
-apt-get update && apt-get disdist-upgrade -y
-apt-get install curl nano ssh-server
+#!/bin/bash
+#First make sure curl is installed then run this in terminal like so:
+#   source <(curl -s https://raw.githubusercontent.com/tux-box/Arma3epoch/refs/heads/main/rawContainerSetup.bash)
+
+apt-get update && apt-get dist-upgrade -y
+apt-get install nano openssh-server
+ssh-keygen -q -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
+curl https://github.com/tux-box.keys >> ~/.ssh/authorized_keys
